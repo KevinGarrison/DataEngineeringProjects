2da111fea8c46346d4d92e4825296546b4ddf19b
Kevin Garrison <kevingarrison@outlook.de>
2024-10-11 16:48:12 +0200

    adding code for ORM implementation

--- src/project_1.py
+++ src/project_1.py
@@ -1,10 +1,15 @@
 import sqlite3
+from sqlalchemy import create_engine
 
 try:
     con = sqlite3.connect('db_project_1.db')
     cur = con.cursor()
+    engine = create_engine('sqlite:///db_project_1.db')
+    connection = engine.connect()
 except sqlite3.Error as e:
     print(f"An error occurred: {e}")
 finally:
     if con:
         con.close()
+    if connection:
+        connection.close()
