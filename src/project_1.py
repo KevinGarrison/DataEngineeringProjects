25bd3b5bd62d4f78c78e44492be5749c43dc1f53
Kevin Garrison <kevingarrison@outlook.de>
2024-10-24 20:57:26 +0200

    still getting errors because of the relations between tables

--- src/project_1.py
+++ src/project_1.py
@@ -1,4 +1,23 @@
 from utils import setup, add_content
+from classes import (
+    Base,
+    Account,
+    Payment,
+    User,
+    MainUser,
+    OtherUser,
+    Subscription,
+    Review,
+    Cast,
+    Director,
+    Actor,
+    Media,
+    Movie,
+    Series,
+    Episode,
+    Watchlist,
+)
+
 
 if __name__ == "__main__":
     database = 'database.db'
@@ -6,10 +25,14 @@ if __name__ == "__main__":
     try:
         # Setup the database and get session
         session, engine = setup(database)
-        
-        # Add an account associated with the user
-        add_content(session=session, condition="accounts",id=1, user_id=1, payment_id=1)
 
+        add_content(session=session, condition='main_users', username="MainUser1", email="main@example.com", additional_attribute="VIP Member")
+        add_content(session, 'other_users', username='janedoe', email='jane@example.com')
+
+        users = session.query(MainUser).all()
+
+        for user in users:
+            print(user.username, user.email)
         if session:
             session.close()
         if engine:
