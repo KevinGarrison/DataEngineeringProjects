b4268251d65253d75b19d78231ff5ab766e5e451
Kevin Garrison <kevingarrison@outlook.de>
2024-10-24 10:30:13 +0200

    imports sqalchemy

--- src/project_1.py
+++ src/project_1.py
@@ -1,5 +1,8 @@
 import sqlite3
-from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 
 try:
     con = sqlite3.connect('db_project_1.db')
