041c393e29e0caf14c0a7ec75f5f9765602e1621
Kevin Garrison <kevingarrison@outlook.de>
2024-10-24 12:46:01 +0200

    add content functions

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-# from classes import Base, alle Classes
+from classes import Base 
 import sqlite3
 
 # Setup Database and Sessionmaker 
@@ -21,14 +21,114 @@ def setup(database_name:str):
     '''
     sqlite3.connect(f'{database_name}')
     engine = create_engine(f'sqlite:///{database_name}')
-    #Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     return Session(), engine
 
-def add_content():
+def add_content(condition:str):
+    match condition:
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+
+
+
+
+def remove_content(condition:str):
+    match condition:
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+        case '':
+            pass
+
+
+def add_account():
     pass
 
-def add_genre():
+def add_subscription_type():
+    pass
+
+def add_payment():
+    pass
+
+def add_user_type():
+    pass
+
+def add_user():
+    pass
+
+def add_main_user():
+    pass
+
+def add_other_user():
+    pass
+
+def add_review():
+    pass
+
+def add_watchlist():
+    pass
+
+def add_cast():
+    pass
+
+def add_media_type():
+    pass
+
+def add_media():
     pass
 
 def add_movie():
@@ -39,6 +139,9 @@ def add_series():
 
 def add_episode():
     pass
+
+
+
 
 
 
