abd03799a504feca62766bbdbd6a9015f1169c44
Nina <nina.scheiffele@studmail.htw-aalen.de>
2024-11-14 10:15:25 +0100

    Comments in output (main)

--- src/main.py
+++ src/main.py
@@ -44,41 +44,50 @@ if __name__ == "__main__":
         tables = inspector.get_table_names()
 
         session, engine = setup(database)
-        #inspector = inspect(engine)
-        #tables = inspector.get_table_names()
-        #for table in tables:
-        #    print(table)
-        #ppd.populate_users(session)
-        #prd.print_users(session)
+        inspector = inspect(engine)
+        tables = inspector.get_table_names()
+        print("Create tables:")
+        for table in tables:
+           print(table)
+        print("These database is filled with:")
+        ppd.populate_users(session)
+        prd.print_users(session)
 
-        #ppd.populate_watchlists(session)
-        #prd.print_watchlists(session)
+        ppd.populate_watchlists(session)
+        prd.print_watchlists(session)
 
         ppd.populate_media(session)
         prd.print_media(session)
 
-        ppd.create_single_cast(session, "src\cast.json")
-        prd.print_cast(session)
-        prd.print_media(session)
-
-
-        #ppd.populate_cast(session)
+        #ppd.create_single_cast(session, "src\cast.json")
         #prd.print_cast(session)
+        #prd.print_media(session)
 
-        #ppd.populate_episodes(session)
-        #prd.print_episodes(session)
-        
-        #ppd.populate_reviews(session)
-        #prd.print_reviews(session)
 
-        #ppd.populate_watchlist_media(session)
-        #prd.print_watchlist_media(session)
+        ppd.populate_cast(session)
+        prd.print_cast(session)
+
+        ppd.populate_episodes(session)
+        prd.print_episodes(session)
         
-        #queries.count_media_per_user(session)
-        #queries.average_rating_by_genre(session)
-        #queries.count_reviews_per_media(session)
-        #queries.total_revenue_by_subscription_type(session)
-        #queries.count_episodes_per_series(session)
+        ppd.populate_reviews(session)
+        prd.print_reviews(session)
+
+        ppd.populate_watchlist_media(session)
+        prd.print_watchlist_media(session)
+
+        print("Here start the queries: ")
+        
+        print("      Count the media per user ")
+        queries.count_media_per_user(session)
+        print("      Calculate the average rating by genre ")
+        queries.average_rating_by_genre(session)
+        print("      Count the reviews per media ")
+        queries.count_reviews_per_media(session)
+        print("      Calculate the revenue by subscription ")
+        queries.total_revenue_by_subscription_type(session)
+        print("      Count the episodes per season ")
+        queries.count_episodes_per_series(session)
 
         if session:
             session.close()
